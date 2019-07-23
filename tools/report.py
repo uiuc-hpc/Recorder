5ad589ec8e231de324dc94d8e26194e674544642
Chen Wang <wangvsa@gmail.com>
2019-07-23 10:17:03 -0700

    Use sorting algorithm to solve the overlap intervals finding problem

--- tools/report.py
+++ tools/report.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 '''
 The report contains the following part:
 1. Number of files
@@ -48,6 +49,7 @@ class TraceReader:
 
 
 def file_statistics(tr: TraceReader, html:HTMLWriter):
+    # 1. Number of file accessed by rank
     fileTable = PrettyTable()
     header = ["", "Total"]
     for r in range(tr.procs): header.append("Rank "+str(r))
@@ -70,9 +72,24 @@ def file_statistics(tr: TraceReader, html:HTMLWriter):
     print(fileTable)
     html.fileTable = fileTable.get_html_string()
 
-    # File sizes image
+    # 2. File access mode
+    accessModeTable = PrettyTable()
+    accessModeTable.field_names = ["File", "Read Only", "Write Only", "Read & Write"]
+    df = tr.get_posix_io()
+    for name in fileSizes.keys():
+        write_only, read_only = True, True
+        if df[(df['filename'] == name) & (df['func'].str.contains("write"))].shape[0] > 0 :
+            read_only = False
+        if df[(df['filename'] == name) & (df['func'].str.contains("read"))].shape[0] > 0 :
+            write_only = False
+        accessModeTable.add_row([name, read_only, write_only, (not read_only) and (not write_only)])
+        html.fileAccessModeTable = accessModeTable.get_html_string()
+    print(accessModeTable)
+
+
+    # 3. File sizes image
     x, y = [], []
-    for name in fileSizes:
+    for name in fileSizes.keys():
         x.append(name)
         y.append(fileSizes[name]/1024.0)
     html.fileSizeImage = "./figures/file_size.jpg"
@@ -121,8 +138,47 @@ def function_statistics(tr: TraceReader, html:HTMLWriter):
 
 
 def offset_statistics(tr: TraceReader, html: HTMLWriter):
-    html.offsetVsRankImage = "./figures/offset_vs_rank.jpg"
-    draw_offset_vs_rank(tr.get_posix_io(), save_to =html.offsetVsRankImage)
+    # 1. Offset vs Ranks image
+    #html.offsetVsRankImage = "./figures/offset_vs_rank.jpg"
+    #draw_offset_vs_rank(tr.get_posix_io(), save_to =html.offsetVsRankImage)
+
+    # 2. Access pattern table
+    accessPatternTable = PrettyTable()
+    accessPatternTable.field_names = ["File", "R->R (self)", "R->R (others)", "R->W (self)", "R->W (others)", \
+                                    "W->R (self)", "W->R (others)", "W->W (self)", "W->W (others)"]
+    df = tr.get_posix_io()
+    for filename in set(df['filename']):
+        records = df[df['filename']==filename][['timestamp', 'offset','count','func','rank']].values.tolist()
+        from operator import itemgetter
+        records = sorted(records, key=itemgetter(1))    # sort by starting position
+
+        # now find ovelapping intervals
+        self_rr, self_rw, self_wr, self_ww = False, False, False, False
+        other_rr, other_rw, other_wr, other_ww = False, False, False, False
+        for i in range(len(records)-1):
+            op1, op2 = records[i], records[i+1]
+            if op2[1] <= op1[1]+op1[2]:     # found it !
+                if op1[4] == op2[4]:    # self
+                    if "read" in op1[3] and "read" in op2[3]: self_rr = True
+                    if "write" in op1[3] and "write" in op2[3]: self_ww = True
+                    if "read" in op1[3] and "write" in op2[3]:
+                        if op1[0] < op2[0]: self_rw = True
+                        else: self_wr = True
+                    if "write" in op1[3] and "read" in op2[3]:
+                        if op1[0] < op2[0]: self_wr = True
+                        else: self_rw = True
+                else:                   # other
+                    if "read" in op1[3] and "read" in op2[3]: other_rr = True
+                    if "write" in op1[3] and "write" in op2[3]: other_ww = True
+                    if "read" in op1[3] and "write" in op2[3]:
+                        if op1[0] < op2[0]: other_rw = True
+                        else: other_wr = True
+                    if "write" in op1[3] and "read" in op2[3]:
+                        if op1[0] < op2[0]: other_wr = True
+                        else: other_rw = True
+        accessPatternTable.add_row([filename, self_rr, other_rr, self_rw, other_rw, self_wr, other_wr, self_ww, other_ww])
+    print(accessPatternTable)
+    html.accessPatternTable = accessPatternTable.get_html_string()
 
 if __name__ == "__main__":
     html = HTMLWriter("./simple_report.html")
