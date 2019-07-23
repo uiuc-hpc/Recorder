32af471b3254c8b648d5d2324224bd511c5ba26d
Chen Wang <wangvsa@gmail.com>
2019-07-23 10:17:33 -0700

    Add some titles

--- tools/html_writer.py
+++ tools/html_writer.py
@@ -27,10 +27,12 @@ class HTMLWriter:
     def __init__(self, path):
         self.path = path
         self.fileTable = ""
+        self.fileAccessModeTable = ""
         self.fileSizeImage = ""
         self.functionTable = ""
         self.functionCountImage = ""
         self.offsetVsRankImage = ""
+        self.accessPatternTable = ""
         self.ioSizesImage = ""
 
     def write_html(self):
@@ -38,21 +40,31 @@ class HTMLWriter:
         <html>
             <head> %s </head>
             <body>
-                <h3> 1. File Statistics </h3>
-                %s </br>
-                <img src="%s" alt="File Size" width="500"></img>
-                <h3> 2. Function Statistics </h3>
-                <img src="%s" alt="Function Count" width="500"></img>
+                <h2> 1. File Statistics </h2>
+                <h4> 1.1 Number of file access by each rank</h4>
                 %s
-                <h3> 3. Offset VS Rank </h3>
+                <h4> 1.2 Access mode of each file </h4>
+                %s
+                <h4>1.3 File Sizes</h4>
+                <img src="%s" alt="File Size" width="500"></img>
+
+                <h2> 2. Function Statistics </h3>
+                <h4> 2.1 Funciton classifications </h4>
+                <img src="%s" alt="Function Count" width="500"></img>
+                <h4> 2.2 Fcount count </h4>
+                %s
+
+                <h2> 3. Offset VS Rank </h2>
                 <img src="%s" alt="offset vs rank" width="600"></img>
-                <h4> 4. Histogram of access sizes </h4>
-                <img src="%s" alt="histogram of access sizes" width="500"></img>
+                %s
+
+                <h2> 4. Percentage of I/O access sizes </h2>
+                <img src="%s" alt="access sizes" width="500"></img>
             </body>
         </html>
-        """ %(css_style, self.fileTable, self.fileSizeImage,    \
+        """ %(css_style, self.fileTable, self.fileAccessModeTable, self.fileSizeImage,    \
                 self.functionCountImage, self.functionTable,    \
-                self.offsetVsRankImage, self.ioSizesImage)
+                self.offsetVsRankImage, self.accessPatternTable, self.ioSizesImage)
 
         f = open("./simple_report.html", "w")
         f.write(html_content)
