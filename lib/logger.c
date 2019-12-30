3fbb9aafb2fc35bbe08aa7245b785af74b89fe35
Chen Wang <wangvsa@gmail.com>
2019-12-29 20:59:35 -0600

    Reconstruct the logger fields

--- lib/logger.c
+++ lib/logger.c
@@ -7,37 +7,42 @@
 #include "zlib.h"
 
 
-// Global file handler (per rank) for the local trace log file
-FILE *__datafh;
-FILE *__metafh;
-
-// Starting timestamp of each rank
-double START_TIMESTAMP;
-double TIME_RESOLUTION = 0.000001;
-
-// Filename to integer map
-hashmap_map *__filename2id_map;
-
-// set to true after initialization (log_init) and before exit()
-bool __recording;
+#define TIME_RESOLUTION 0.000001
+#define RECORD_WINDOW_SIZE 3        // A sliding window for peephole compression
+#define ZLIB_BUF_SIZE 4096          // For zlib compression //
 
 
-/* A sliding window for peephole compression */
-#define RECORD_WINDOW_SIZE 3
-Record __record_window[RECORD_WINDOW_SIZE];
+struct Logger {
+    FILE *dataFile;             // log file
+    FILE *metaFile;             // metadata file
 
-/* For zlib compression */
-#define ZLIB_BUF_SIZE 4096
-z_stream __zlib_stream;
+    bool recording;             // set to true only after initialization
 
-/* compression mode */
-CompressionMode __compression_mode = COMP_ZLIB;
+    double startTimestamp;
+
+    RecorderLocalDef localDef;  // Local metadata information
+    hashmap_map *fileMap;       // Filename to Integer map
+
+    CompressionMode compMode;   // Compression Mode
+
+    // For Recorder Compression Mode
+    Record recordWindow[RECORD_WINDOW_SIZE];
+
+    // For Zlib
+    z_stream zlibStream;
+};
+// Global object to access the Logger fileds
+struct Logger __logger;
 
 
-RecorderLocalDef __local_def;
 
 
-// Buffer the tracing records. Dump when its full
+/**
+ * ------------------------------------------------------------- //
+ *
+ * Memery buffer to hold records. Write out when its full
+ *
+ */
 struct MemBuf {
     void *buffer;
     int size;
@@ -46,14 +51,17 @@ struct MemBuf {
     void (*append)(struct MemBuf*, const void* ptr, int length);
     void (*dump) (struct MemBuf*);
 };
+
+// global object to access the MemBuf methods
+struct MemBuf __membuf;
+
 void membufRelease(struct MemBuf *membuf) {
     free(membuf->buffer);
     membuf->pos = 0;
 }
 void membufAppend(struct MemBuf* membuf, const void *ptr, int length) {
-    printf("append\n");
     if (length >= membuf->size) {
-        RECORDER_REAL_CALL(fwrite) (ptr, 1, length, __datafh);
+        RECORDER_REAL_CALL(fwrite) (ptr, 1, length, __logger.dataFile);
         return;
     }
     if (membuf->pos + length >= membuf->size) {
@@ -63,8 +71,7 @@ void membufAppend(struct MemBuf* membuf, const void *ptr, int length) {
     membuf->pos += length;
 }
 void membufDump(struct MemBuf *membuf) {
-    printf("dump\n");
-    RECORDER_REAL_CALL(fwrite) (membuf->buffer, 1, membuf->pos, __datafh);
+    RECORDER_REAL_CALL(fwrite) (membuf->buffer, 1, membuf->pos, __logger.dataFile);
     membuf->pos = 0;
 }
 void membufInit(struct MemBuf* membuf) {
@@ -75,7 +82,10 @@ void membufInit(struct MemBuf* membuf) {
     membuf->append = membufAppend;
     membuf->dump = membufDump;
 }
-struct MemBuf __membuf;
+// --------------- End of Memory Buffer ------------------------ //
+
+
+
 
 
 static inline int startsWith(const char *pre, const char *str) {
@@ -147,8 +157,8 @@ static inline void writeInText(FILE *f, Record record) {
 
 // Mode 2. Write in binary format, no compression
 static inline void writeInBinary(FILE *f, Record record) {
-    int tstart = (record.tstart - START_TIMESTAMP) / TIME_RESOLUTION;
-    int tend   = (record.tend - START_TIMESTAMP) / TIME_RESOLUTION;
+    int tstart = (record.tstart - __logger.startTimestamp) / TIME_RESOLUTION;
+    int tend   = (record.tend - __logger.startTimestamp) / TIME_RESOLUTION;
     __membuf.append(&__membuf, &(record.status), sizeof(char));
     __membuf.append(&__membuf, &tstart, sizeof(int));
     __membuf.append(&__membuf, &tend, sizeof(int));
@@ -165,7 +175,7 @@ static inline void writeInRecorder(FILE* f, Record new_record) {
     int min_diff_count = 999;
     char ref_window_id;
     for(int i = 0; i < RECORD_WINDOW_SIZE; i++) {
-        Record record = __record_window[i];
+        Record record = __logger.recordWindow[i];
         // Only meets the following conditions that we consider to compress it:
         // 1. same function as the one in sliding window
         // 2. has at least 1 arguments
@@ -193,15 +203,15 @@ static inline void writeInRecorder(FILE* f, Record new_record) {
         diff_record.tstart = new_record.tstart;
         diff_record.tend = new_record.tend;
         diff_record.func_id = ref_window_id;
-        writeInBinary(__datafh, diff_record);
+        writeInBinary(__logger.dataFile, diff_record);
     } else {
         new_record.status = 0b00000000;
-        writeInBinary(__datafh, new_record);
+        writeInBinary(__logger.dataFile, new_record);
     }
 
-    __record_window[2] = __record_window[1];
-    __record_window[1] = __record_window[0];
-    __record_window[0] = new_record;
+    __logger.recordWindow[2] = __logger.recordWindow[1];
+    __logger.recordWindow[1] = __logger.recordWindow[0];
+    __logger.recordWindow[0] = new_record;
 
 }
 
@@ -219,59 +229,58 @@ static inline void writeInZlib(FILE *f, Record record) {
     }
     strcat(in_buf, "\n");
 
-    __zlib_stream.avail_in = strlen(in_buf);
-    __zlib_stream.next_in = in_buf;
+    __logger.zlibStream.avail_in = strlen(in_buf);
+    __logger.zlibStream.next_in = in_buf;
     do {
-        __zlib_stream.avail_out = ZLIB_BUF_SIZE;
-        __zlib_stream.next_out = out_buf;
-        int ret = deflate(&__zlib_stream, Z_NO_FLUSH);    /* no bad return value */
-        unsigned have = ZLIB_BUF_SIZE - __zlib_stream.avail_out;
+        __logger.zlibStream.avail_out = ZLIB_BUF_SIZE;
+        __logger.zlibStream.next_out = out_buf;
+        int ret = deflate(&__logger.zlibStream, Z_NO_FLUSH);    /* no bad return value */
+        unsigned have = ZLIB_BUF_SIZE - __logger.zlibStream.avail_out;
         RECORDER_REAL_CALL(fwrite) (out_buf, 1, have, f);
-    } while (__zlib_stream.avail_out == 0);
+    } while (__logger.zlibStream.avail_out == 0);
 }
 
 void zlib_init() {
     /* allocate deflate state */
-    __zlib_stream.zalloc = Z_NULL;
-    __zlib_stream.zfree = Z_NULL;
-    __zlib_stream.opaque = Z_NULL;
-    deflateInit(&__zlib_stream, Z_DEFAULT_COMPRESSION);
+    __logger.zlibStream.zalloc = Z_NULL;
+    __logger.zlibStream.zfree = Z_NULL;
+    __logger.zlibStream.opaque = Z_NULL;
+    deflateInit(&__logger.zlibStream, Z_DEFAULT_COMPRESSION);
 }
 void zlib_exit() {
     // Write out everythin zlib's buffer
     char out_buf[ZLIB_BUF_SIZE];
     do {
 
-        __zlib_stream.avail_out = ZLIB_BUF_SIZE;
-        __zlib_stream.next_out = out_buf;
-        int ret = deflate(&__zlib_stream, Z_FINISH);    /* no bad return value */
-        unsigned have = ZLIB_BUF_SIZE - __zlib_stream.avail_out;
-        RECORDER_REAL_CALL(fwrite) (out_buf, 1, have, __datafh);
-    } while (__zlib_stream.avail_out == 0);
+        __logger.zlibStream.avail_out = ZLIB_BUF_SIZE;
+        __logger.zlibStream.next_out = out_buf;
+        int ret = deflate(&__logger.zlibStream, Z_FINISH);    /* no bad return value */
+        unsigned have = ZLIB_BUF_SIZE - __logger.zlibStream.avail_out;
+        RECORDER_REAL_CALL(fwrite) (out_buf, 1, have, __logger.dataFile);
+    } while (__logger.zlibStream.avail_out == 0);
     // Clean up and end the Zlib
-    (void)deflateEnd(&__zlib_stream);
+    (void)deflateEnd(&__logger.zlibStream);
 }
 
 
+void write_record(Record record) {
+    if (!__logger.recording) return;       // have not initialized yet
 
-void write_record(Record new_record) {
-    if (!__recording) return;       // have not initialized yet
+    __logger.localDef.total_records++;
+    __logger.localDef.function_count[record.func_id]++;
 
-    __local_def.total_records++;
-    __local_def.function_count[new_record.func_id]++;
-
-    switch(__compression_mode) {
+    switch(__logger.compMode) {
         case COMP_TEXT:
-            writeInText(__datafh, new_record);
+            writeInText(__logger.dataFile, record);
             break;
         case COMP_BINARY:
-            writeInBinary(__datafh, new_record);
+            writeInBinary(__logger.dataFile, record);
             break;
         case COMP_ZLIB:
-            writeInZlib(__datafh, new_record);
+            writeInZlib(__logger.dataFile, record);
             break;
         default:
-            writeInRecorder(__datafh, new_record);
+            writeInRecorder(__logger.dataFile, record);
             break;
     }
 }
@@ -288,29 +297,29 @@ void logger_init(int rank, int nprocs) {
     MAP_OR_FAIL(mkdir)
 
     // Initialize the global values
-    __filename2id_map = hashmap_new();
+    __logger.fileMap = hashmap_new();
 
-    START_TIMESTAMP = recorder_wtime();
+    __logger.startTimestamp = recorder_wtime();
 
     RECORDER_REAL_CALL(mkdir) ("logs", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     char logfile_name[256];
     char metafile_name[256];
     sprintf(logfile_name, "logs/%d.itf", rank);
     sprintf(metafile_name, "logs/%d.mt", rank);
-    __datafh = RECORDER_REAL_CALL(fopen) (logfile_name, "wb");
-    __metafh = RECORDER_REAL_CALL(fopen) (metafile_name, "wb");
+    __logger.dataFile = RECORDER_REAL_CALL(fopen) (logfile_name, "wb");
+    __logger.metaFile = RECORDER_REAL_CALL(fopen) (metafile_name, "wb");
 
     // Global metadata, include compression mode, time resolution
     const char* comp_mode = getenv("RECORDER_COMPRESSION_MODE");
-    if (comp_mode) __compression_mode = atoi(comp_mode);
-    if (__compression_mode == COMP_ZLIB)        // Initialize zlib if compression mode is COMP_ZLIB
+    if (comp_mode) __logger.compMode = atoi(comp_mode);
+    if (__logger.compMode == COMP_ZLIB)        // Initialize zlib if compression mode is COMP_ZLIB
         zlib_init();
     if (rank == 0) {
         FILE* global_metafh = RECORDER_REAL_CALL(fopen) ("logs/recorder.mt", "wb");
         RecorderGlobalDef global_def = {
             .time_resolution = TIME_RESOLUTION,
             .total_ranks = nprocs,
-            .compression_mode = __compression_mode,
+            .compression_mode = __logger.compMode,
             .peephole_window_size = RECORD_WINDOW_SIZE
         };
         RECORDER_REAL_CALL(fwrite)(&global_def, sizeof(RecorderGlobalDef), 1, global_metafh);
@@ -318,55 +327,55 @@ void logger_init(int rank, int nprocs) {
     }
 
     membufInit(&__membuf);
-    __recording = true;
+    __logger.recording = true;
 }
 
 
 void logger_exit() {
-    __recording = false;
+    __logger.recording = false;
 
     /* Call this before close file since we still could have data in zlib's buffer waiting to write out*/
-    if (__compression_mode == COMP_ZLIB)
+    if (__logger.compMode == COMP_ZLIB)
         zlib_exit();
 
 
     /* Write out local metadata information */
-    __local_def.num_files = hashmap_length(__filename2id_map),
-    __local_def.start_timestamp = START_TIMESTAMP,
-    __local_def.end_timestamp = recorder_wtime(),
-    RECORDER_REAL_CALL(fwrite) (&__local_def, sizeof(__local_def), 1, __metafh);
+    __logger.localDef.num_files = hashmap_length(__logger.fileMap),
+    __logger.localDef.start_timestamp = __logger.startTimestamp,
+    __logger.localDef.end_timestamp = recorder_wtime(),
+    RECORDER_REAL_CALL(fwrite) (&__logger.localDef, sizeof(__logger.localDef), 1, __logger.metaFile);
 
     /* Write out filename mappings, we call stat() to get file size
-     * since __datafh is already closed (null), the stat() function
+     * since is already closed (null), the stat() function
      * won't be intercepted. */
-    if (hashmap_length(__filename2id_map) > 0 ) {
-        for(int i = 0; i< __filename2id_map->table_size; i++) {
-            if(__filename2id_map->data[i].in_use != 0) {
-                char *filename = __filename2id_map->data[i].key;
-                int id = __filename2id_map->data[i].data;
+    if (hashmap_length(__logger.fileMap) > 0 ) {
+        for(int i = 0; i< __logger.fileMap->table_size; i++) {
+            if(__logger.fileMap->data[i].in_use != 0) {
+                char *filename = __logger.fileMap->data[i].key;
+                int id = __logger.fileMap->data[i].data;
                 size_t file_size = get_file_size(filename);
                 int filename_len = strlen(filename);
-                RECORDER_REAL_CALL(fwrite) (&id, sizeof(id), 1, __metafh);
-                RECORDER_REAL_CALL(fwrite) (&file_size, sizeof(file_size), 1, __metafh);
-                RECORDER_REAL_CALL(fwrite) (&filename_len, sizeof(filename_len), 1, __metafh);
-                RECORDER_REAL_CALL(fwrite) (filename, sizeof(char), filename_len, __metafh);
+                RECORDER_REAL_CALL(fwrite) (&id, sizeof(id), 1, __logger.metaFile);
+                RECORDER_REAL_CALL(fwrite) (&file_size, sizeof(file_size), 1, __logger.metaFile);
+                RECORDER_REAL_CALL(fwrite) (&filename_len, sizeof(filename_len), 1, __logger.metaFile);
+                RECORDER_REAL_CALL(fwrite) (filename, sizeof(char), filename_len, __logger.metaFile);
             }
         }
     }
 
-    hashmap_free(__filename2id_map);
-    __filename2id_map = NULL;
-    if ( __metafh) {
-        RECORDER_REAL_CALL(fclose) (__metafh);
-        __metafh = NULL;
+    hashmap_free(__logger.fileMap);
+    __logger.fileMap = NULL;
+    if ( __logger.metaFile) {
+        RECORDER_REAL_CALL(fclose) (__logger.metaFile);
+        __logger.metaFile = NULL;
     }
 
 
     __membuf.dump(&__membuf);
     __membuf.release(&__membuf);
     /* Close the log file */
-    if ( __datafh ) {
-        RECORDER_REAL_CALL(fclose) (__datafh);
-        __datafh = NULL;
+    if ( __logger.dataFile) {
+        RECORDER_REAL_CALL(fclose) (__logger.dataFile);
+        __logger.dataFile = NULL;
     }
 }
