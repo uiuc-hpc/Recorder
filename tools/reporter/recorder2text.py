312c5b4a2c3429f52819164a49dd21f21a5ea2ff
Chen Wang <wangvsa@gmail.com>
2020-02-18 16:21:22 -0600

    Add recroder to text format converter

---
+++ tools/reporter/recorder2text.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from reader import RecorderReader
+from reader import func_list
+import sys, os
+
+def fill_in_filename(record, fileMap):
+    func = func_list[record[3]]
+    args = record[4]
+
+    if "H5" in func or "MPI" in func:
+        return record
+
+    if "fwrite" in func or "fread" in func:
+        fileId = int(args[3])
+        filename = fileMap[fileId][2]
+        record[4][3] = filename
+    elif "seek" in func or "open" in func or "write" in func or "read" in func:
+        fileId = int(args[0])
+        filename = fileMap[fileId][2]
+        record[4][0] = filename
+    return record
+
+if __name__ == "__main__":
+    logs_dir = sys.argv[1]+"/../logs_text"
+    try:
+        os.mkdir(logs_dir)
+    except:
+        pass
+
+    reader = RecorderReader(sys.argv[1])
+    for rank in range(reader.globalMetadata.numRanks):
+        fileMap = reader.localMetadata[rank].fileMap
+
+        print(rank, len(reader.records[rank]))
+        with open(logs_dir+"/"+str(rank)+".txt", 'w') as f:
+            for record in reader.records[rank]:
+                record = fill_in_filename(record, fileMap)
+                recordText = "%s %s %s %s\n" %(record[1], record[2], func_list[record[3]], record[4])
+                f.write(recordText)
