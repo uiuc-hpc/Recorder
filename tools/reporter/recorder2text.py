b8df100b65757cf659d5986f26148a02cb10f8fc
Chen Wang <wangvsa@gmail.com>
2020-03-02 16:49:47 -0600

    Fill in name for mmap

--- tools/reporter/recorder2text.py
+++ tools/reporter/recorder2text.py
@@ -11,7 +11,11 @@ def fill_in_filename(record, fileMap, func_list):
     if "H5" in func or "MPI" in func:
         return record
 
-    if "fwrite" in func or "fread" in func:
+    if "mmap" in func:
+        fileId = int(args[4])
+        filename = fileMap[fileId][2]
+        recorder[4][4] = filename
+    elif "fwrite" in func or "fread" in func:
         fileId = int(args[3])
         filename = fileMap[fileId][2]
         record[4][3] = filename
