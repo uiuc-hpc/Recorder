37bdd5034c59c98489992bfe8c41bc029694cada
Chen Wang <wangvsa@gmail.com>
2020-03-02 20:59:15 -0600

    Fix typo

--- tools/reporter/recorder2text.py
+++ tools/reporter/recorder2text.py
@@ -14,7 +14,7 @@ def fill_in_filename(record, fileMap, func_list):
     if "mmap" in func:
         fileId = int(args[4])
         filename = fileMap[fileId][2]
-        recorder[4][4] = filename
+        record[4][4] = filename
     elif "fwrite" in func or "fread" in func:
         fileId = int(args[3])
         filename = fileMap[fileId][2]
