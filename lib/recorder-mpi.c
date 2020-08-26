7413fb938bdf2114ddbe945123a280835fe21dd2
Chen Wang <wangvsa@gmail.com>
2020-08-26 09:39:30 -0500

    Set communicator name in MPI_Comm_dup

--- lib/recorder-mpi.c
+++ lib/recorder-mpi.c
@@ -570,6 +570,12 @@ int RECORDER_MPI_DECL(MPI_Comm_create) (MPI_Comm comm, MPI_Group group, MPI_Comm
 
 int RECORDER_MPI_DECL(MPI_Comm_dup) (MPI_Comm comm, MPI_Comm * newcomm) {
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_Comm_dup, (comm, newcomm));
+
+    char new_comm_name[128] = {0};
+    char* parent_name = comm2name(comm);
+    sprintf(new_comm_name, "%s_dup", parent_name);
+    PMPI_Comm_set_name(*newcomm, new_comm_name);
+
     char **args = assemble_args_list(2, comm2name(comm), comm2name(*newcomm));
     RECORDER_INTERCEPTOR(2, args);
 }
