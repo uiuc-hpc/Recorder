064ff7c73b467a02fa9ff447bca4c80a576edcfd
Chen Wang <wangvsa@gmail.com>
2022-04-12 19:19:15 -0700

    Filter out mpi-io calls according to the filename

--- lib/recorder-mpi.c
+++ lib/recorder-mpi.c
@@ -78,8 +78,9 @@ typedef struct MPICommHash_t {
 } MPICommHash;
 
 typedef struct MPIFileHash_t {
-    void *key;
+    void* key;
     char* id;
+    int   accept;   // Should we intercept calls for this file
     UT_hash_handle hh;
 } MPIFileHash;
 
@@ -88,7 +89,8 @@ static MPIFileHash *mpi_file_table = NULL;
 static int mpi_file_id = 0;
 static int mpi_comm_id = 0;
 
-void add_mpi_file(MPI_Comm comm, MPI_File *file) {
+
+void add_mpi_file(MPI_Comm comm, MPI_File *file, CONST char* filename) {
     if(file == NULL)
         return;
 
@@ -105,6 +107,9 @@ void add_mpi_file(MPI_Comm comm, MPI_File *file) {
         sprintf(id, "%d-%d", world_rank, mpi_file_id++);
     PMPI_Bcast(id, 32, MPI_BYTE, 0, comm);
     entry->id = id;
+    char* tmp_filename = realrealpath(filename);
+    entry->accept = accept_filename(tmp_filename);
+    free(tmp_filename);
 
     HASH_ADD_KEYPTR(hh, mpi_file_table, entry->key, sizeof(MPI_File), entry);
 }
@@ -121,6 +126,19 @@ char* file2id(MPI_File *file) {
             return strdup("MPI_FILE_UNKNOWN");
     }
 }
+
+/*
+ * Check if we should intercept an MPI-IO call according
+ * to the inclusion or exclusion file list
+ */
+#define FILTER_MPIIO_CALL(func, func_args, fh)                      \
+    MPIFileHash *entry = NULL;                                      \
+    HASH_FIND(hh, mpi_file_table, fh, sizeof(MPI_File), entry);     \
+    if(!entry || !entry->accept) {                                  \
+        MAP_OR_FAIL(func);                                          \
+        return RECORDER_REAL_CALL(func) func_args;                  \
+    }
+
 
 // Return the relative rank in the new communicator
 int add_mpi_comm(MPI_Comm *newcomm) {
@@ -347,7 +365,8 @@ int RECORDER_MPI_IMP(MPI_Type_commit) (MPI_Datatype *datatype) {
 
 int RECORDER_MPI_IMP(MPI_File_open) (MPI_Comm comm, CONST char *filename, int amode, MPI_Info info, MPI_File *fh) {
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_open, (comm, filename, amode, info, fh));
-    add_mpi_file(comm, fh);
+    add_mpi_file(comm, fh, filename);
+    // TODO incorporate FILTER_MPIIO_CALL here
     char **args = assemble_args_list(5, comm2name(&comm), realrealpath(filename), itoa(amode), ptoa(&info), file2id(fh));
     RECORDER_INTERCEPTOR(5, args);
 }
@@ -362,6 +381,7 @@ int RECORDER_MPI_IMP(MPI_File_close) (MPI_File *fh) {
         free(entry->key);
         free(entry);
     }
+    // TODO incorporate FILTER_MPIIO_CALL here
 
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_close, (fh));
     char **args = assemble_args_list(1, fid);
@@ -369,72 +389,84 @@ int RECORDER_MPI_IMP(MPI_File_close) (MPI_File *fh) {
 }
 
 int RECORDER_MPI_IMP(MPI_File_sync) (MPI_File fh) {
+    FILTER_MPIIO_CALL(PMPI_File_sync, (fh), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_sync, (fh));
     char **args = assemble_args_list(1, file2id(&fh));
     RECORDER_INTERCEPTOR(1, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_set_size) (MPI_File fh, MPI_Offset size) {
+    FILTER_MPIIO_CALL(PMPI_File_set_size, (fh, size), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_set_size, (fh, size));
     char **args = assemble_args_list(2, file2id(&fh), itoa(size));
     RECORDER_INTERCEPTOR(2, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_set_view) (MPI_File fh, MPI_Offset disp, MPI_Datatype etype, MPI_Datatype filetype, CONST char *datarep, MPI_Info info) {
+    FILTER_MPIIO_CALL(PMPI_File_set_view, (fh, disp, etype, filetype, datarep, info), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_set_view, (fh, disp, etype, filetype, datarep, info));
     char **args = assemble_args_list(6, file2id(&fh), itoa(disp), type2name(etype), type2name(filetype), ptoa(datarep), ptoa(&info));
     RECORDER_INTERCEPTOR(6, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read) (MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_at) (MPI_File fh, MPI_Offset offset, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read_at, (fh, offset, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_at, (fh, offset, buf, count, datatype, status));
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(6, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_at_all) (MPI_File fh, MPI_Offset offset, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read_at_all, (fh, offset, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_at_all, (fh, offset, buf, count, datatype, status));
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(6, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_all) (MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read_all, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_all, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_shared) (MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read_shared, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_shared, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_ordered) (MPI_File fh, void *buf, int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_read_ordered, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_ordered, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_at_all_begin) (MPI_File fh, MPI_Offset offset, void *buf, int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_read_at_all_begin, (fh, offset, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_at_all_begin, (fh, offset, buf, count, datatype));
     char **args = assemble_args_list(5, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_all_begin) (MPI_File fh, void *buf, int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_read_all_begin, (fh, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_all_begin, (fh, buf, count, datatype));
     char **args = assemble_args_list(4, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(4, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_read_ordered_begin) (MPI_File fh, void *buf, int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_read_ordered_begin, (fh, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_read_ordered_begin, (fh, buf, count, datatype));
     char **args = assemble_args_list(4, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(4, args);
@@ -442,6 +474,7 @@ int RECORDER_MPI_IMP(MPI_File_read_ordered_begin) (MPI_File fh, void *buf, int c
 
 int RECORDER_MPI_IMP(MPI_File_iread_at) (MPI_File fh, MPI_Offset offset, void *buf, int count,
         MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iread_at, (fh, offset, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iread_at, (fh, offset, buf, count, datatype, request));
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(6, args);
@@ -449,6 +482,7 @@ int RECORDER_MPI_IMP(MPI_File_iread_at) (MPI_File fh, MPI_Offset offset, void *b
 
 int RECORDER_MPI_IMP(MPI_File_iread) (MPI_File fh, void *buf, int count,
         MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iread, (fh, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iread, (fh, buf, count, datatype, request));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(5, args);
@@ -456,6 +490,7 @@ int RECORDER_MPI_IMP(MPI_File_iread) (MPI_File fh, void *buf, int count,
 
 int RECORDER_MPI_IMP(MPI_File_iread_shared) (MPI_File fh, void *buf, int count,
         MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iread_shared, (fh, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iread_shared, (fh, buf, count, datatype, request));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(5, args);
@@ -463,6 +498,7 @@ int RECORDER_MPI_IMP(MPI_File_iread_shared) (MPI_File fh, void *buf, int count,
 
 int RECORDER_MPI_IMP(MPI_File_write) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
@@ -470,17 +506,15 @@ int RECORDER_MPI_IMP(MPI_File_write) (MPI_File fh, CONST void *buf, int count,
 
 int RECORDER_MPI_IMP(MPI_File_write_at) (MPI_File fh, MPI_Offset offset, CONST void *buf,
         int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write_at, (fh, offset, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_at, (fh, offset, buf, count, datatype, status));
-
-    // CHEN here
-    offset = 0;
-
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(6, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_write_at_all) (MPI_File fh, MPI_Offset offset, CONST void *buf,
         int count, MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write_at_all, (fh, offset, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_at_all, (fh, offset, buf, count, datatype, status));
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(6, args);
@@ -488,6 +522,7 @@ int RECORDER_MPI_IMP(MPI_File_write_at_all) (MPI_File fh, MPI_Offset offset, CON
 
 int RECORDER_MPI_IMP(MPI_File_write_all) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write_all, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_all, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
@@ -495,6 +530,7 @@ int RECORDER_MPI_IMP(MPI_File_write_all) (MPI_File fh, CONST void *buf, int coun
 
 int RECORDER_MPI_IMP(MPI_File_write_shared) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write_shared, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_shared, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
@@ -502,6 +538,7 @@ int RECORDER_MPI_IMP(MPI_File_write_shared) (MPI_File fh, CONST void *buf, int c
 
 int RECORDER_MPI_IMP(MPI_File_write_ordered) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, MPI_Status *status) {
+    FILTER_MPIIO_CALL(PMPI_File_write_ordered, (fh, buf, count, datatype, status), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_ordered, (fh, buf, count, datatype, status));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), status2str(status));
     RECORDER_INTERCEPTOR(5, args);
@@ -509,18 +546,21 @@ int RECORDER_MPI_IMP(MPI_File_write_ordered) (MPI_File fh, CONST void *buf, int 
 
 int RECORDER_MPI_IMP(MPI_File_write_at_all_begin) (MPI_File fh, MPI_Offset offset, CONST void *buf,
         int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_write_at_all_begin, (fh, offset, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_at_all_begin, (fh, offset, buf, count, datatype));
     char **args = assemble_args_list(5, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_write_all_begin) (MPI_File fh, CONST void *buf, int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_write_all_begin, (fh, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_all_begin, (fh, buf, count, datatype));
     char **args = assemble_args_list(4, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(4, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_write_ordered_begin) (MPI_File fh, CONST void *buf, int count, MPI_Datatype datatype) {
+    FILTER_MPIIO_CALL(PMPI_File_write_ordered_begin, (fh, buf, count, datatype), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_write_ordered_begin, (fh, buf, count, datatype));
     char **args = assemble_args_list(4, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype));
     RECORDER_INTERCEPTOR(4, args);
@@ -528,6 +568,7 @@ int RECORDER_MPI_IMP(MPI_File_write_ordered_begin) (MPI_File fh, CONST void *buf
 
 int RECORDER_MPI_IMP(MPI_File_iwrite_at) (MPI_File fh, MPI_Offset offset, CONST void *buf,
         int count, MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iwrite_at, (fh, offset, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iwrite_at, (fh, offset, buf, count, datatype, request));
     char **args = assemble_args_list(6, file2id(&fh), itoa(offset), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(6, args);
@@ -535,6 +576,7 @@ int RECORDER_MPI_IMP(MPI_File_iwrite_at) (MPI_File fh, MPI_Offset offset, CONST 
 
 int RECORDER_MPI_IMP(MPI_File_iwrite) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iwrite, (fh, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iwrite, (fh, buf, count, datatype, request));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(5, args);
@@ -542,24 +584,28 @@ int RECORDER_MPI_IMP(MPI_File_iwrite) (MPI_File fh, CONST void *buf, int count,
 
 int RECORDER_MPI_IMP(MPI_File_iwrite_shared) (MPI_File fh, CONST void *buf, int count,
         MPI_Datatype datatype, __D_MPI_REQUEST *request) {
+    FILTER_MPIIO_CALL(PMPI_File_iwrite_shared, (fh, buf, count, datatype, request), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_iwrite_shared, (fh, buf, count, datatype, request));
     char **args = assemble_args_list(5, file2id(&fh), ptoa(buf), itoa(count), type2name(datatype), ptoa(request));
     RECORDER_INTERCEPTOR(5, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_seek) (MPI_File fh, MPI_Offset offset, int whence) {
+    FILTER_MPIIO_CALL(PMPI_File_seek, (fh, offset, whence), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_seek, (fh, offset, whence));
     char **args = assemble_args_list(3, file2id(&fh), itoa(offset), whence2name(whence));
     RECORDER_INTERCEPTOR(3, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_seek_shared) (MPI_File fh, MPI_Offset offset, int whence) {
+    FILTER_MPIIO_CALL(PMPI_File_seek_shared, (fh, offset, whence), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_seek_shared, (fh, offset, whence));
     char **args = assemble_args_list(3, file2id(&fh), itoa(offset), whence2name(whence));
     RECORDER_INTERCEPTOR(3, args);
 }
 
 int RECORDER_MPI_IMP(MPI_File_get_size) (MPI_File fh, MPI_Offset *offset) {
+    FILTER_MPIIO_CALL(PMPI_File_get_size, (fh, offset), &fh);
     RECORDER_INTERCEPTOR_NOIO(int, PMPI_File_get_size, (fh, offset));
     char **args = assemble_args_list(2, file2id(&fh), itoa(*offset));
     RECORDER_INTERCEPTOR(2, args);
