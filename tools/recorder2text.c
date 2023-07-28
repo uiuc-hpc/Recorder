3f2f5ac178857e5faad3fe1e7974b18e5e87f4db
Chen Wang <wangvsa@gmail.com>
2023-07-28 11:06:15 -0700

    Formatting text trace filenames
    Signed-off-by: Chen Wang <wangvsa@gmail.com>

--- tools/recorder2text.c
+++ tools/recorder2text.c
@@ -10,7 +10,17 @@
 #define DECIMAL 6
 
 RecorderReader reader;
-static char formating_string[32];
+static char formatting_record[32];
+static char formatting_fname[20];
+
+int digits_count(int n) {
+    int digits = 0;
+    do {
+        n /= 10;
+        ++digits;
+    } while (n != 0);
+    return digits;
+}
 
 void write_to_textfile(Record *record, void* arg) {
     FILE* f = (FILE*) arg;
@@ -19,7 +29,7 @@ void write_to_textfile(Record *record, void* arg) {
 
     const char* func_name = recorder_get_func_name(&reader, record);
 
-    fprintf(f, formating_string, record->tstart, record->tend, // record->tid
+    fprintf(f, formatting_record, record->tstart, record->tend, // record->tid
                              func_name, record->level, recorder_get_func_type(&reader, record));
 
     for(int arg_id = 0; !user_func && arg_id < record->arg_count; arg_id++) {
@@ -52,7 +62,8 @@ int main(int argc, char **argv) {
     recorder_init_reader(argv[1], &reader);
 
     int decimal =  log10(1 / reader.metadata.time_resolution);
-    sprintf(formating_string, "%%.%df %%.%df %%s %%d %%d (", decimal, decimal);
+    sprintf(formatting_record, "%%.%df %%.%df %%s %%d %%d (", decimal, decimal);
+    sprintf(formatting_fname,  "%%s/%%0%dd.txt", digits_count(reader.metadata.total_ranks));
 
     // Each rank will process n files (n ranks traces)
     int n = max(reader.metadata.total_ranks/mpi_size, 1);
@@ -61,7 +72,7 @@ int main(int argc, char **argv) {
 
     for(int rank = start_rank; rank < end_rank; rank++) {
 
-        sprintf(textfile_path, "%s/%d.txt", textfile_dir, rank);
+        sprintf(textfile_path, formatting_fname, textfile_dir, rank);
         FILE* fout = fopen(textfile_path, "w");
 
         recorder_decode_records(&reader, rank, write_to_textfile, fout);
