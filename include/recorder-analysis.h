ea234384872b7710799e6194f8b81ed0354e2eec
Hammad Bin Ather <ather1@lassen708.coral.llnl.gov>
2024-07-24 11:17:32 -0700

    I/O knowledge extraction

--- include/recorder-analysis.h
+++ include/recorder-analysis.h
@@ -11,6 +11,30 @@ typedef struct RuleConfidence_t {
     UT_hash_handle hh;
 } RuleConfidence;
 
-int recorder_analysis(RecorderLogger* logger, Record* record, CallSignature* entry);
+int recorder_analysis(RecorderLogger* logger, Record* record, CallSignature* entry, Knowledge *knowledge);
 
+static const char* collective_func_list[] = {
+    // MPI 16 functions
+    "MPI_Barrier",                  "MPI_Bcast",                        "MPI_Gather",                   "MPI_Scan",
+    "MPI_Gatherv",                  "MPI_Scatter",                      "MPI_Scatterv",                 "MPI_Reduce",
+    "MPI_Allreduce",                "MPI_Reduce_scatter",               "MPI_File_write_all",           "MPI_File_write_at_all_begin",
+    "MPI_File_write_at_all",        "MPI_File_read_all_begin",          "MPI_File_read_all",            "MPI_File_read_at_all",
+
+
+    // HDF5 I/O - 54 functions
+    "H5Acreate",                "H5Acreate1",           "H5Acreate2",           "H5Acreate_by_name",
+    "H5Adelete",                "H5Adelete_by_idx",     "H5Adelete_by_name",    "H5Arename", 
+    "H5Arename_by_name",        "H5Awrite",             "H5Dcreate",            "H5Dcreate1",
+    "H5Dcreate2",               "H5Dcreate_anon",       "H5Dextend",            "H5Dset_extent",
+    "H5Fclose",                 "H5Fcreate",            "H5Fflush",             "H5Fmount",
+    "H5Fopen",                  "H5Freopen",            "H5Funmount",           "H5Gcreate",
+    "H5Gcreate1",               "H5Gcreate2",           "H5Gcreate_anon",       "H5Glink",
+    "H5Glink2",                 "H5Gmove",              "H5Gmove2",             "H5Gset_comment",
+    "H5Gunlink",                "H5Idec_ref",           "H5LIinc_ref",          "H5Lcopy",
+    "H5Lcreate_external",       "H5Lcreate_hard",       "H5Lcreate_soft",       "H5Lcreate_ud",
+    "H5Ldelete",                "H5Ldelete_by_idx",     "H5Lmove",              "H5Ocopy",
+    "H5Odecr_refcount",         "H5Oincr_refcount",     "H5Olink",              "H5Oset_comment",
+    "H5Oset_comment_by_name",   "H5Rcreate",            "H5Tcommit",            "H5Tcommit1",
+    "H5Tcommit2",               "H5Tcommit_anon"
+};
 #endif 
--- lib/recorder-analysis.c
+++ lib/recorder-analysis.c
@@ -1,12 +1,15 @@
+#include <stdio.h>
+#include <sqlite3.h> 
 #include "recorder-analysis.h"
 #include "mpi.h"
+
 
 #define SIZE 100
 #define THRESHOLD 4
 
 static RuleConfidence* cfg_conf = NULL;
+
 static int top = -1;
-char pattern[512];
 
 void push(int x, int stack[])
 {
@@ -43,7 +46,7 @@ int custom_cmp(Symbol* matched_rule, int val){
     return 1;
 }
 
-void extract_io_behavior(RecorderLogger* logger, int stack[]){
+void extract_io_behavior(RecorderLogger* logger, int stack[], Knowledge *knowledge){
     int tmp_stack[SIZE];
     int tmp_top = top;
 
@@ -52,41 +55,41 @@ void extract_io_behavior(RecorderLogger* logger, int stack[]){
     }
 
     int terminal_id = 0;
-    for (int i = tmp_top; i >= 0; --i){
+    knowledge->collective = 0;
+    sprintf(knowledge->spatial_locality, "random");
+
+    for (int i = 0; i <= tmp_top; i++){
         terminal_id = tmp_stack[i];
-        tmp_top = tmp_top - 1;
+        
         CallSignature *s;
         for (s = logger->cst; s != NULL; s = (CallSignature *)(s->hh.next)) {
             if (s->terminal_id == terminal_id){
                 Record * record = cs_to_record(s);
                 const char * func_name = get_function_name_by_id(record->func_id);
-                if (strcmp(func_name, "MPI_File_write") == 0){
-                    sprintf(pattern, "%s",  "MPI_File_write");
-                    // logger->pattern = "write";
-                    printf("Pattern %s \n", pattern);
-                    // char file_name[50];
-                    // sprintf(file_name, ".%d.io-pattern.txt", logger->rank);
-                    // FILE *fptr;
-                    // fptr = fopen(file_name, "a");
-                    // if (fptr) {
-                    //     fprintf(fptr, "write");         
-                    //     fclose(fptr);
-                    // }
-                    // printf("Key %p, Terminal %d\n", s->key, s->terminal_id);
+                
+                printf("Function name: %s\n", func_name);
+                printf("Terminal ID: %d\n", terminal_id);
+                printf("Printing Args\n\n");
+                
+                for (int i = 0; i < record->arg_count; i++){
+                    printf(record->args[i]);
+                    printf("\n");
                 }
-
-                printf("Function Name %s\n", func_name);
-
-                // printf("Args Count: %d\n", record->arg_count);
-
-                // for (int i = 0; i < record->arg_count; i++){
-                //     printf(record->args[i]);      
-                //     printf("\n");
-                // }
+                printf("\nFinished printing Args\n");
+                // Extract collective I/O knowledge
+                
+                if (strcmp(func_name, "MPI_File_write_at_all") == 0){
+                    knowledge->collective = 1;
+                }
+                if (strcmp(func_name, "MPI_File_write_at") == 0){
+                    knowledge->collective = 0;
+                }
+                if (strcmp(func_name, "write") == 0){
+                    sprintf(knowledge->file_name, record->args[0]);
+                }
             }  
         } 
     }
-    printf("\n\n\n");
 }
 
 void find_rule_confidence(RecorderLogger* logger, CallSignature *entry){
@@ -212,7 +215,7 @@ int find_pattern_start(RecorderLogger* logger, int search_value, bool initial, i
     return 0;
 }
 
-void analysis_cs(RecorderLogger* logger, CallSignature *entry){
+void analysis_cs(RecorderLogger* logger, CallSignature *entry, Knowledge *knowledge){
     Symbol *rule1, *sym1;
     int rules_count1 = 0, symbols_count1 = 0;
     DL_COUNT(logger->cfg.rules, rule1, rules_count1);
@@ -272,43 +275,132 @@ void analysis_cs(RecorderLogger* logger, CallSignature *entry){
         }
 
         if (max_rule_count > THRESHOLD){
-            printf("Chosen rule %d, count %d, terminal ID %d , Rank %d\n", max_rule, max_rule_count, entry->terminal_id, logger->rank);
+            printf("Chosen rule %d, count %d, terminal ID %d , Rank %d\n\n", max_rule, max_rule_count, entry->terminal_id, logger->rank);
             find_pattern_start(logger, max_rule, false, stack);
         }
     }
 
-    // show(stack);
     if (top != -1){
-        extract_io_behavior(logger, stack);
+        extract_io_behavior(logger, stack, knowledge);
     }
 }
 
-int recorder_analysis(RecorderLogger* logger,  Record* record, CallSignature* entry) {
+int recorder_analysis(RecorderLogger* logger,  Record* record, CallSignature* entry, Knowledge *knowledge) {
+    
+    
     const char * func_name = get_function_name_by_id(record->func_id);
-    RECORDER_LOGINFO("Function Name %s\n", func_name);
-
-    if(strcmp("MPI_File_close", func_name) == 0){
-        int *sub_avgs = NULL;
-        if (logger->rank == 0) {
-            sub_avgs = (int *) recorder_malloc(sizeof(int) * logger->nprocs);
+    size_t len = sizeof(collective_func_list) / sizeof(char *);
+    
+    unsigned char i;
+    bool is_collective = false;
+    for(i = 0; i < len; i++) {
+        if (strcmp(collective_func_list[i], func_name) == 0){
+            is_collective = true;
+            break;
         }
-
-        // RECORDER_LOGINFO("PMPI_Gather start with rank %d\n", logger->rank);
-        GOTCHA_REAL_CALL(MPI_Gather)(&(logger->current_cfg_terminal), 1, MPI_INT, sub_avgs, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        // RECORDER_LOGINFO("PMPI_Gather end with rank %d\n", logger->rank);
-        // if (logger->rank == 0){
-        //     for(int i = 0; i < logger->nprocs; i++){
-        //         RECORDER_LOGINFO("Val %d :-> ", sub_avgs[i]);
-        //     }
-        //     printf("\n");        
-        // }
-
-        if (logger->rank == 0)
-            recorder_free(sub_avgs, logger->nprocs);
     }
 
-    find_rule_confidence(logger, entry);
-    analysis_cs(logger, entry);
+    if(is_collective){
+        // int * all_pattern_ids;
+        // if (logger->rank == 0) {
+        //     all_pattern_ids = (int *) recorder_malloc(sizeof(int) * logger->nprocs);
+        // }
+        // PMPI_Gather(&logger->pattern_id, 1, MPI_INT, all_pattern_ids, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        
+        // if (logger->rank == 0){
+        //     int max_pattern_id = 0;
+        //     for (int i = 0; i < logger->nprocs; i++){
+        //         RECORDER_LOGINFO("Val %d :-> ", all_pattern_ids[i]);
+        //     }
 
+        //     int maxcount = 0; 
+        //     int element_having_max_freq; 
+        //     for (int i = 0; i < logger->nprocs; i++) { 
+        //         int count = 0; 
+        //         for (int j = 0; j < logger->nprocs; j++) { 
+        //             if (all_pattern_ids[i] == all_pattern_ids[j]) 
+        //                 count++; 
+        //         } 
+        
+        //         if (count > maxcount) { 
+        //             maxcount = count; 
+        //             element_having_max_freq = all_pattern_ids[i]; 
+        //         } 
+        //     }
+        //     RECORDER_LOGINFO("MAX  %d :-> ", element_having_max_freq);
+        // }
+        
+        // if (logger->rank == 0)
+        //     recorder_free(all_pattern_ids, logger->nprocs);  
+                
+        int* recvcounts = NULL;
+        if (logger->rank == 0){
+            recvcounts = (int *) recorder_malloc(logger->nprocs*sizeof(int)); 
+        }
+        int mylen = strlen(knowledge->file_name); 
+        
+        MPI_Gather(&mylen,1,MPI_INT,recvcounts,1,MPI_INT,0,MPI_COMM_WORLD); 
+        
+        int totlen = 0; 
+        int* displs = NULL;
+        char* totalstring = NULL;
+
+        if(logger->rank == 0)
+        {   
+            displs = (int *) recorder_malloc(logger->nprocs*sizeof(int));
+            displs[0] = 0; 
+            totlen += recvcounts[0] + 1; 
+            for(int i=1; i < logger->nprocs; i++)
+            {
+                totlen += recvcounts[i]+1; 
+                displs[i] = displs[i-1] + recvcounts[i-1] + 1; 
+            }
+            
+            totalstring = (char *) recorder_malloc(totlen*sizeof(char)); 
+            for (int i = 0; i < totlen - 1; i++) {
+                totalstring[i] = ' '; 
+            
+            totalstring[totlen - 1] = '\0'; 
+            }
+        }
+    
+        MPI_Gatherv(&knowledge->file_name, mylen, MPI_CHAR,
+                    totalstring, recvcounts, displs, MPI_CHAR,
+                    0, MPI_COMM_WORLD);
+
+
+        if (logger->rank == 0){
+            char * token = strtok(totalstring, " ");
+            char * first_token = token;
+            bool shared_file = true;
+            while( token != NULL ) {
+                printf( " %s\n", token ); 
+                token = strtok(NULL, " ");
+                if (token != NULL){
+                    if (strcmp(first_token, token) != 0){
+                        shared_file = false;
+                        break;
+                    }
+                } 
+            }
+
+            if (!shared_file){
+                sprintf(knowledge->file_operation, "file-per-process");
+            } else {
+                sprintf(knowledge->file_operation, "shared_file");
+            }
+
+            printf("Knowledge for rank %d, Collective I/O %d, Spatial Locality %s, File Operation %s \n", logger->rank, knowledge->collective, knowledge->spatial_locality, knowledge->file_operation);
+
+            free(totalstring);
+            free(displs);
+            free(recvcounts);
+
+        }
+   
+    }
+    find_rule_confidence(logger, entry);
+    analysis_cs(logger, entry, knowledge);    
+    
     return 0;
 }
